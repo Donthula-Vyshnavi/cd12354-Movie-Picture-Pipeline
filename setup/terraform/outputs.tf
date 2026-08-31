7f09840ef5ea4e6aa55c5ffcd165ea5810c785de
Donthula Vyshnavi <vyshhnavi.donthula@gmail.com>
2026-08-31 13:10:32 +0000

    Add backend deployment workflow and update Terraform configuration

--- setup/terraform/outputs.tf
+++ setup/terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "frontend_ecr" {
   value = aws_ecr_repository.frontend.repository_url
 }
+
 output "backend_ecr" {
   value = aws_ecr_repository.backend.repository_url
 }
@@ -13,6 +14,3 @@ output "cluster_version" {
   value = aws_eks_cluster.main.version
 }
 
-output "github_action_user_arn" {
-  value = aws_iam_user.github_action_user.arn
-}
