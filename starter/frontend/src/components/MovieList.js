62849ae9c8b63a3ddcba25532d65ae44d47eb674
Donthula-Vyshnavi <vyshhnavi.donthula@gmail.com>
2026-09-01 06:14:56 +0000

    Fix movie list API response handling

---
+++ starter/frontend/src/components/MovieList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+function MovieList({ onMovieClick }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.error('Error fetching movies:', error);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+          {movie.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+MovieList.propTypes = {
+  onMovieClick: PropTypes.func.isRequired,
+};
+
+export default MovieList;
